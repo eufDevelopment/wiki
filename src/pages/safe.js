3760e6fae0841fb45f0481a2a8fb73eb11d24272
Gabrieles <1528366+gabrieles@users.noreply.github.com>
2021-04-21 01:12:42 +0100

    Create safe.js

---
+++ src/pages/safe.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ComponentName = () => (
+
+    <Layout> 
+        <h1>Safe Sport Report Form</h1>
+        <div>TBD...</div>
+    </Layout> 
+)
+
+export default ComponentName
