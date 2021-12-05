a45084a37293a73b2fc4d77bfd9f12d757ac3a9c
Gabrieles <1528366+gabrieles@users.noreply.github.com>
2021-12-05 02:43:55 +0000

    empty search

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,27 @@
-//import React, { useState } from 'react'
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Fuse from "fuse.js";
 
+const searchPage = () => (
+    <StaticQuery
+      query={graphql`
+        {
+            siteSearchIndex(fuse: {}) {
+                fuse
+            }
+        }
+      `}
+      render={data => (
+      <Layout> 
+        <Seo title="Search" keywords={[`gender equity`, `gender equality`, `ultimate`, `coaching`, `European Ultimate Federation`, `erasmus`]}  />
+        <main>
+          
+          
+        </main>
+      </Layout> )}
+    ></StaticQuery>
+  )
 
-//export default SearchPage
+export default searchPage
